792570cd632b3f44c459ab28d05f3505e14d1c7e
filodex <36601598+filodex@users.noreply.github.com>
2022-10-21 14:55:41 +0300

    idk what im doing

---
+++ client/src kris/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react'
+import 'materialize-css'
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                Любимка моя, поехали за кольцами
+            </header>
+        </div>
+    )
+}
+export default App
+
+//DEFAULT
--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import './App.css'
 import React from 'react'
 import 'materialize-css'
+import { useRoutes } from './routes'
+import { BrowserRouter } from 'react-router-dom'
+import { PricesPage } from './pages/PricesPage'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { WelcomePage } from './pages/WelcomePage'
 
 function App() {
+    //const routes = useRoutes()
     return (
-        <div className='App'>
-            <header className='App-header'>
-                Любимка моя, поехали за кольцами
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/prices' element={<PricesPage />} />
+                <Route path='/' element={<WelcomePage />} />
+                <Route path='*' element={<Navigate to='/' />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 export default App
 
-//DEFAULT
+//   <div className='container'>{routes}</div>
