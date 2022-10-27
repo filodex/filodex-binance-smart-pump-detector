ed49b2066721a8164f6414cb099bb8e903c2e378
filodex <36601598+filodex@users.noreply.github.com>
2022-10-27 23:21:09 +0300

    out to prices

--- client/src/pages/PricesPage.js
+++ client/src/pages/PricesPage.js
@@ -3,22 +3,55 @@ import { Route } from 'react-router-dom'
 import { useHttp } from '../hooks/http.hook.js'
 
 function PricesList(props) {
-    return <div>{props.prices}</div>
+    console.log(props)
+    /**
+     * BTCUSDT deviation: 1,213123
+     */
+    if (props.prices['1min']) {
+        let liList = []
+
+        for (const iterator of props.prices['1min'].up) {
+            liList.push(
+                <li>
+                    {iterator.ticker} deviation: {iterator.deviation}
+                </li>
+            )
+        }
+
+        return <ul>{liList}</ul>
+        // return (
+        //     <ul>
+        //         <li>
+        //             {props.prices['1min'].up[0].ticker} deviation:
+        //             {props.prices['1min'].up[0].deviation}
+        //         </li>
+        //         <li>
+        //             {props.prices['1min'].up[1].ticker} deviation:
+        //             {props.prices['1min'].up[1].deviation}
+        //         </li>
+        //         <li>
+        //             {props.prices['1min'].up[2].ticker} deviation:
+        //             {props.prices['1min'].up[2].deviation}
+        //         </li>
+        //     </ul>
+        // )
+    }
+
+    return 'nothing at now'
 }
 
 export const PricesPage = () => {
     const [pricesValue, setPricesValue] = useState('no prices')
     const { loading, request } = useHttp()
-    const pricesHandler = () => {
-        setPricesValue('it works')
+
+    const pricesHandler = async () => {
+        try {
+            const data = await request('/api/prices/lastknown')
+            console.log(data)
+            setPricesValue(data.data)
+            return
+        } catch (error) {}
     }
-    // const pricesHandler = async () => {
-    //     try {
-    //         const data = await request('/api/prices/lastknown')
-    //         console.log(data)
-    //         return await data.json()
-    //     } catch (error) {}
-    // }
 
     return (
         <div>
