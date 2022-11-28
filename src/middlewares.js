3b63e358bbd71fde1b018ee6a06a5b39dc8d3866
filodex <filodex@gmail.com>
2022-11-28 17:57:38 +0300

    adding stats metric

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,10 +1,63 @@
 import express from 'express'
+import { logger } from './winston.js'
 
+let requestsCount = 0
+let requestsCountADay = 0
+
+// on /api call
 export function getStatistics(req, res, next) {
-    console.log(`middleware отработал`)
-    console.log(
-        'res ==========================================\n',
-        express.json(req)()
-    )
+    requestsCount++
+    requestsCountADay++
+    // Statistics
+    const apiCall = new ApiCall({
+        reqDate: new Date(),
+        reqId: requestsCount,
+    })
+    console.log(apiCall)
+
     next()
+}
+
+// count when midnight starts
+let nowDay = new Date()
+// console.log(`nowDay ${nowDay}`)
+let nextDay = new Date(
+    nowDay.getFullYear(),
+    nowDay.getMonth(),
+    nowDay.getDate() + 1
+)
+// console.log(`nextDay ${nextDay}`)
+let diff = nextDay - nowDay
+// console.log(`diff ${diff}`)
+console.log(
+    `timeout that starts startDayStatisticsInterval() started, new day in ${diff}`
+)
+
+setTimeout(() => {
+    startDayStatisticsInterval()
+}, diff)
+
+// start this at midnight
+function startDayStatisticsInterval() {
+    console.log(`New day started!!!`)
+    logger.info(
+        `New day started!!!, there were ${requestsCountADay} requests this day, and ${requestsCount} requests at all`
+    )
+    requestsCountADay = 0
+    // once a day write requestsCountADay to file and make it 0
+    let dayStatisticsInterval = setInterval(() => {
+        // write to file
+        console.log(`New day started!!!`)
+        logger.info(
+            `New day started!!!, there were ${requestsCountADay} requests this day, and ${requestsCount} requests at all`
+        )
+        requestsCountADay = 0
+    }, 86400000) // 86400000 ms on a day
+}
+
+class ApiCall {
+    constructor({ reqDate, reqId }) {
+        this.reqDate = reqDate || new Date()
+        this.reqId = reqId || undefined
+    }
 }
