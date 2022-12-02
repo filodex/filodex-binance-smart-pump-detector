8945cc4eaf234ad94d3776dbc2964e3c8cf0cbc6
filodex <filodex@gmail.com>
2022-12-02 17:56:24 +0300

    controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -5,6 +5,31 @@ class UserController {
             console.log(error.message)
         }
     }
+    async login(req, res, next) {
+        try {
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    async logout(req, res, next) {
+        try {
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    async refresh(req, res, next) {
+        try {
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    async getUsers(req, res, next) {
+        try {
+            res.json([1, 2])
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 }
 
 export default new UserController()
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
+import userController from '../controllers/user-controller.js'
 
 const router = new Router()
 
 // /auth/...
-router.post('/login')
-router.post('/logout')
-router.post('/registration')
-router.post('/refresh')
-router.post('/test')
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
+router.post('/registration', userController.registration)
+router.get('/refresh', userController.refresh)
+router.get('/users', userController.getUsers)
 
 export default router
